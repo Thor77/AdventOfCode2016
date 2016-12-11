3529f5247aa4b3a07603bf93bf6e42aaa52a1723
Thor77 <thor77@thor77.org>
2016-12-11 20:18:14 +0100

    Add solution in python for day5 part2

--- day05.py
+++ day05.py
@@ -13,3 +13,19 @@ while len(password) < 8:
     i += 1
 
 print('Password:', password)
+
+password_p2 = [None] * 8
+i = 0
+while None in password_p2:
+    hexed = md5((door_id + str(i)).encode('utf-8')).hexdigest()
+    if hexed.startswith('00000'):
+        try:
+            position = int(hexed[5])
+        except ValueError:
+            i += 1
+            continue
+        char = hexed[6]
+        if position < len(password_p2) and password_p2[position] is None:
+            password_p2[position] = char
+    i += 1
+print('Password (Part2):', ''.join(password_p2))
