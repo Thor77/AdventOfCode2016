0c1ee8ec98a74717863e25b9c83ed44c2b4fb9cb
Thor77 <thor77@thor77.org>
2016-12-19 21:42:28 +0100

    Replace namedtupe with list of lists
    for performance-improvements, as the replace-
    operation is not neccessary anymore

--- day19.py
+++ day19.py
@@ -1,27 +1,21 @@
-from collections import namedtuple
-
-Elf = namedtuple('Elf', ['number', 'presents'])
-
-
 def solve(elf_count):
     # give all elves their initial present
-    elves = [Elf(i, presents=1) for i in range(elf_count)]
+    elves = [[i, 1] for i in range(elf_count)]
     while len(elves) > 1:
         for i in range(len(elves)):
             if i >= len(elves):
                 break
             # find elf next to elf i with presents on hand
             next_elf = (i + 1) % len(elves)
-            while elves[next_elf].presents == 0:
+            while elves[next_elf][1] == 0:
                 if next_elf + 1 >= len(elves):
                     break
                 next_elf = (next_elf + 1) % len(elves)
             # take presents from next elf
-            elves[i] = elves[i]._replace(
-                presents=elves[i].presents + elves[next_elf].presents
-            )
+            elves[i][1] += elves[next_elf][1]
             del elves[next_elf]
-    return elves[0].number + 1
+        print(len(elves), 'elves left after this round')
+    return elves[0][0] + 1
 
 if __name__ == '__main__':
     elf_count = 0
