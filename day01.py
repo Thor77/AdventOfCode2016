1109fa7b18a846e513631fee9e006f6813bb5a1f
Thor77 <thor77@thor77.org>
2017-07-25 19:55:23 +0200

    Add solution for day01p2

--- day01.py
+++ day01.py
@@ -12,8 +12,13 @@ def calculate_distance(x, y):
     return x + y
 
 
+def inclusive_range(start, stop, step=1):
+    return range(start, (stop + 1) if step >= 0 else (stop - 1), step)
+
+
 orientation = 1  # N, E, S, W
 x, y = 0, 0
+locations = [(x, y)]
 
 for instruction in instructions:
     # parse instruction
@@ -32,15 +37,45 @@ for instruction in instructions:
     if orientation < 1:
         orientation = 4
 
+    # save previous position
+    previous_x = x
+    previous_y = y
+
     # move
+    steps = []
     if orientation == 1:
         y += distance
+        steps = map(
+            lambda s: (x, s),
+            inclusive_range(previous_y, y)
+        )
     elif orientation == 2:
         x += distance
+        steps = map(
+            lambda s: (s, y),
+            inclusive_range(previous_x, x)
+        )
     elif orientation == 3:
         y -= distance
+        steps = map(
+            lambda s: (x, s),
+            inclusive_range(previous_y, y, -1)
+        )
     elif orientation == 4:
         x -= distance
+        steps = map(
+            lambda s: (s, y),
+            inclusive_range(previous_x, x, -1)
+        )
+
+    for step in list(steps)[1:]:
+        if step in locations:
+            print(
+                'Part 2:', step,
+                'Distance:', calculate_distance(step[0], step[1])
+            )
+        else:
+            locations.append(step)
 
 print('Position:', (x, y))
 print('Distance:', calculate_distance(x, y))
