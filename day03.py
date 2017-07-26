26861098d228704c22323b379f7bab58d78d8fc6
Thor77 <thor77@thor77.org>
2017-07-26 13:31:16 +0200

    Refactor day03 to isolate is_possible-check

--- day03.py
+++ day03.py
@@ -1,19 +1,20 @@
 from itertools import permutations
 
-triangles = []
-with open('input/03') as f:
-    triangles = [
-        (int(s1), int(s2), int(s3))
-        for s1, s2, s3 in [line.rstrip('\n').strip().split() for line in f]
-    ]
 
-possible_triangles = 0
-
-for triangle in triangles:
+def is_possible(triangle):
     for s1, s2, s3 in permutations(triangle):
         if not (s1 + s2) > s3:
-            break
+            return False
     else:
-        possible_triangles += 1
+        return True
 
-print(possible_triangles, 'triangles are possible')
+
+if __name__ == '__main__':
+    triangles = []
+    with open('input/03') as f:
+        triangles = [
+            (int(s1), int(s2), int(s3))
+            for s1, s2, s3 in [line.rstrip('\n').strip().split() for line in f]
+        ]
+    possible_triangles = filter(is_possible, triangles)
+    print(len(list(possible_triangles)), 'triangles are possible')
