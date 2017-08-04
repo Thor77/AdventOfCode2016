83e40a6bee109bd0fc4aa1f7020eb949e8a40654
Thor77 <thor77@thor77.org>
2017-08-04 23:26:37 +0200

    Add WIP for day07p2

--- day07.py
+++ day07.py
@@ -58,3 +58,24 @@ for address_sequences, hypernet_sequences in addresses:
         supporting_tls += 1
 
 print(supporting_tls, 'out of', len(addresses), 'support tls')
+
+
+def find_aba_or_bab(text):
+    for c1, c2, c3 in window(text, size=3):
+        if c1 == c3 and c1 != c2:
+            yield c1, c2
+    else:
+        return [(None, None)]
+
+
+supporting_ssl = 0
+for address_sequences, hypernet_sequences in addresses:
+    aba_s_in_address = sum(list(map(list, (map(find_aba_or_bab, address_sequences)))), [])
+    bab_s_in_hypernet = sum(list(map(list, (map(find_aba_or_bab, hypernet_sequences)))), [])
+    for a_char, b_char in aba_s_in_address:
+        if not a_char or not b_char:
+            continue
+        if (b_char, a_char) in bab_s_in_hypernet:
+            supporting_ssl += 1
+
+print(supporting_ssl, 'support ssl')
